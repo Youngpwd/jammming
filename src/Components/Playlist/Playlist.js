f5ed21579be7a0640b97c7e9592f0fe33876ebdc
Charles Young <cbyoung415@gmail.com>
2022-06-27 15:08:49 -0700

    add track and tracklist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Playlist.css";
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Playlist extends React.Component {
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
         {/* <!-- Add a TrackList component --> */}
+        <TrackList />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TrackList";
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+      </div>
+    );
+  }
+}
+
+export default TrackList;
