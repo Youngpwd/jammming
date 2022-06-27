62d362843b32d38260e58464f105341a145d077b
Charles Young <cbyoung415@gmail.com>
2022-06-27 09:02:06 -0700

    changed App to class component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,26 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import { SearchBar } from "../SearchBar/SearchBar";
+import SearchBar from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
 import { Playlist } from "../Playlist/Playlist";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Ja<span className="highlight">mmm</span>ing
-      </h1>
-      <div className="App">
-        {/* <!-- Add a SearchBar component --> */}
-        <div className="App-playlist">
-          {/* <!-- Add a SearchResults component -->
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component -->
         <!-- Add a Playlist component --> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
